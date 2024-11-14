38c4f7dbc2ab5f2014f54d6fa18b36c0ed0214c2
guizinho-fluzao <guilherme@infocentro.com.br>
2024-11-13 22:16:28 -0300

    Update sistema_de_batalha.py

--- sistema_de_batalha.py
+++ sistema_de_batalha.py
@@ -1,127 +1,74 @@
-import personagens as pers
+import pygame
+import random
 
-def criar_aliado(): # CRIADOR DE HERÓI
-    print('DE QUAL CLASSE SEU PERSONAGEM SERÁ?')
-    print('''          [1] GUERREIRO
-          [2] ASSASSINO
-          [3] TANQUE
-          [4] CURANDEIRO
-          [5] MAGO
-          [6] BARDO''')
-    
-    classes = [pers.Guerreiro, pers.Assassino, pers.Tanque, pers.Curandeiro, pers.Mago, pers.Bardo]
-    
-    while True:
-        escolha = int(input("ESCOLHA UMA CLASSE: ")) - 1
-        if 0 <= escolha <= 5:
-            classe = classes[escolha]
-            break
-        else:
-            print("ESCOLHA INVÁLIDA. TENTE NOVAMENTE. ")
-    
-    nome = input("ESCOLHA O NOME DO PERSONAGEM: ")
+pygame.init()
 
-    return classe(nome)
+class Atacante:
+    def _init_(self):
+        self.nome = "Kael, o Guardião das Sombras"
+        self.ataque = 100
+        self.vida = 1000
+        self.defesa = 50
+        self.velocidade = 2
 
-def criar_aliados(): # GERADOR DO TIME DE HERÓIS
+class Defensor:
+    def _init_(self):
+        self.nome = "Lyra, a Arqueira da Aurora"
+        self.ataque = 50
+        self.vida = 1000
+        self.defesa = 300
+        self.velocidade = 1
 
-    Aliados = []
+class Flash:
+    def _init_(self):
+        self.nome = "Finn, o Mago das Chamas"
+        self.ataque = 60
+        self.vida = 1000
+        self.defesa = 50
+        self.velocidade = 3
 
-    for i in range(3):
-        print('CRIE O ALIADO ' + str(i + 1))
-        Aliados.append(criar_aliado())
-        Aliados[i].mostrarStats()
-        input('APERTE QUALQUER BOTÃO...')
+class AtacanteInimigo:
+    def _init_(self):
+        self.nome = "Malakar, o Ceifador Sombrio"
+        self.ataque = 100
+        self.vida = 1000
+        self.defesa = 50
+        self.velocidade = 2
 
-    return Aliados
+class DefensorInimigo:
+    def _init_(self):
+        self.nome = "Thorne, o Brutamontes das Trevas"
+        self.ataque = 50
+        self.vida = 1000
+        self.defesa = 300
+        self.velocidade = 1
 
-def criar_inimigos(): # GERADOR DOS INIMIGOS
-
-    Inimigos = []
-
-    for i in range(3):
-        Nome = 'Inimigo ' + str(i + 1)
-        Inimigos.append(pers.Inimigo(Nome))
-
-    return Inimigos
-
-def ordenaVelocidade(personagens): # ORDENADOR DA ORDEM DA BATALHA
-    personagens.sort(reverse = True, key= lambda p: p.velocidade)
-    print([str(personagem) for personagem in personagens])
-
-def verificaVivos(personagens): # VERIFICA QUAIS PERSONAGENS ESTÃO VIVOS
-    for i in personagens[:]:
-        if i.vida <= 0:
-            print(f'{i.nome} MORREU')
-            personagens.remove(i)
-
-def verificar(personagens): # VERIFICAÇÃO DE VIVOS E DA SITUAÇÃO DA BATALHA. USADO PARA DEFINIR A VITÓRIA/DERROTA
-    
-    verificaVivos(personagens)
-
-    AliadosVivos = 0
-    InimigosVivos = 0
-    Resultado = 0
-
-    for i in personagens:
-        if i.ehInimigo == False:
-            AliadosVivos += 1
-        else:
-            InimigosVivos += 1
-    
-    if AliadosVivos == 0:
-        Resultado = 1
-    if InimigosVivos == 0:
-        Resultado = 2
-
-    if Resultado == 1 or 2:
-        fimBatalha(Resultado)
-        return True
-    else:
-        return False
-        
-
-def fimBatalha(resultado):
-    if resultado == 1:
-        print('INIMIGOS VENCERAM! VOCÊ FOI DERROTADO!')
-    elif resultado == 2:
-        print('HERÓIS VENCERAM! VOCÊ VENCEU!')
-    else:
-        return None
-    
-
-    
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ CLASSES ^ ~~~~ CÓDIGO v ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
+class FlashInimigo:
+    def _init_(self):
+        self.nome = "Voss, o Mestre das Poções"
+        self.ataque = 60
+        self.vida = 1000
+        self.defesa = 50
+        self.velocidade = 3
 
 
-def simular():
-    Aliados = criar_aliados()
-    Inimigos = criar_inimigos()
+jogador_personagens = [Atacante(), Defensor(), Flash()]
+inimigo_personagens = [AtacanteInimigo(), DefensorInimigo(), FlashInimigo()]
 
-    Personagens = Aliados + Inimigos # JUNTA TODOS OS PERSONAGENS EM UMA SÓ LISTA. USADA TAMBÉM PARA SABER QUEM ESTÁ VIVO
-    print([str(personagem) for personagem in Personagens])
-    ordenaVelocidade(Personagens)
 
-    while True:
+def calcular_dano(atacante, defensor):
+    dano = atacante.ataque * (50/(50+defensor.defesa))
+    defensor.vida -= dano 
+    return max(dano,0)
 
-        # INÍCIO DE UMA RODADA
+def ordenar_personagens(personagens):
+    return sorted(personagens, key=lambda x: x.velocidade, reverse=True)
 
-        ordenaVelocidade(Personagens)
+running=True
+personagens_ordenados = ordenar_personagens(jogador_personagens + inimigo_personagens)
+indice_turno=0
 
-        for personagem in Personagens: # PARA CADA PERSONAGEM VIVO
 
-            personagem.vida = 0
-            
-            # rodar escolha de ação
-
-            if verificar(Personagens):
-                break
-
-        else: 
-            continue
-
-        break
-
-    return None
-
-simular()
+while running:
+    for event in pygame.event.get():
+        if event.type
