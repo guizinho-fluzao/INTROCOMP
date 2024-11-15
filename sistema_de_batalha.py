89c5edb19e175bce3a55c7d5ee3c32148cbb1155
guizinho-fluzao <guilherme@infocentro.com.br>
2024-11-15 20:40:12 -0300

    Update sistema_de_batalha.py

--- sistema_de_batalha.py
+++ sistema_de_batalha.py
@@ -79,9 +79,54 @@ def verificar(personagens): # VERIFICAÇÃO DE VIVOS E DA SITUAÇÃO DA BATALHA.
         return True
     else:
         return False
-        
 
-def fimBatalha(resultado):
+def atualizaVida(self, dano): # ATUALIZA A VIDA DO PERSONAGEM APÓS UM CONFRONTO
+    if dano > 0 and self.vidaAtual + dano > self.vidaMaxima:
+        self.vidaAtual = self.vidaMaxima
+    elif dano < 0 and self.vidaAtual + dano <= 0:
+        self.vidaAtual = 0
+    else:
+        self.vidaAtual += dano
+
+    print(f"Vida atual de {self.nome}: {self.vidaAtual}")
+
+def estarSeDefendendo(self): # INSPECIONA SE O PERSONAGEM ATUAL ESTÁ SE DEFENDENDO
+    return self.defendendo
+
+def defesa(self): # QUANDO É ESCOLHIDA A OPÇÃO DEFESA
+    if not self.estarSeDefendendo():
+        self.DEF *= 2
+        self.defendendo = True
+
+def pararDeDefender(self): # QUANDO O PERSONAGEM PARA DE SE DEFENDER
+    if self.estarSeDefendendo():
+        self.DEF = int (self.DEF * 0.5)
+        self.defendendo = False
+
+def calculaDano (): # CÁLCULO DO DANO QUE O PERSONAGEM RECEBEU DO OPONENTE
+        
+def ataque(personagemAtual, Aliados, Inimigos): # QUANDO É ESCOLHIDA A OPÇÃO ATAQUE
+    selecaoInimigo = 0
+
+    while True:
+        print("Selecione o inimigo para atacar:")
+        for i in range(len(Inimigos)):
+            print(f"[{i} - {Inimigos[i].nome}]")
+        print("[-1 - encerrar jogo]")
+
+        selecaoInimigo = int(input())
+
+        if selecaoInimigo == -1:
+            return 0
+        elif selecaoInimigo == 0 or selecaoInimigo == 1:
+
+            # Complete o código aqui!!!
+            calcularDanoAtaqueBasico(personagemAtual, Inimigos[selecaoInimigo])
+            return 1
+        else:
+            print('Opção inválida! Tente novamente')
+
+def fimBatalha(resultado): # MOSTRA O RESULTADO NO FINAL DA PARTIDA
     if resultado == 1:
         print('INIMIGOS VENCERAM! VOCÊ FOI DERROTADO!')
     elif resultado == 2:
@@ -112,7 +157,38 @@ def simular():
 
             personagem.vida = 0
             
-            # rodar escolha de ação
+#fazendo codigo sistema de batalha
+
+            entrada_usuario = input("Pressione A para atacar, D para defender, ou S para sair: ").lower() # ESCOLHA DA AÇÃO DO PERSONAGEM ATUAL
+
+            if entrada_usuario == 's':
+                emExecucao = False
+            elif entrada_usuario == 'a':
+                entradaAtaque = atacar(personagemAtual, Personagens, Inimigos)
+
+                if entradaAtaque == 1:
+                    verificaVivos()
+
+                    if verificaVivos:
+                        listaBatalha.remove(verificaVivos)
+                        print(f"{verificaVivos.nome} foi derrotado!")
+
+                        if verificaVivos in listaPersonagens:
+                            listaPersonagens.remove(verificaVivos)
+                        else:
+                            listaInimigos.remove(verificaVivos)
+                    indiceAtual = proximoIndice(indiceAtual, listaBatalha)
+                    personagemAtual = listaBatalha[indiceAtual]
+
+                else:
+                    emExecucao = False
+
+            elif entrada_usuario == 'd':
+                personagemAtual.defender()
+                indiceAtual = proximoIndice(indiceAtual, listaBatalha)
+                personagemAtual = listaBatalha[indiceAtual]
+
+#termino codigo sistema de batalha
 
             if verificar(Personagens):
                 break
